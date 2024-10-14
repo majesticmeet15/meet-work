48b37fb0b60165ac5b17e808776202ddc351da2d
majesticmeet15 <meetvanodia15@outlook.com>
2024-10-14 13:10:07 +0530

    work

---
+++ assingnments/module 4.2/task 14,4.2 .cpp
@@ -0,0 +1,102 @@
+/*Define a class to represent a bank account. Include the following members: 
+
+ Data Member:
+-Name of the depositor
+-Account Number 
+-Type of Account 
+-Balance amount in the account 
+ 
+Member Functions 
+-To assign values 
+-To deposited an amount 
+-To withdraw an amount after checking balance 
+-To display name and balance */
+
+
+#include<iostream>
+using namespace std;
+
+class Bank
+{
+	private:
+	//Data Members
+	string name;			//Name of the depositor 
+	long long accountno;	//Account Number 
+	string account_type;	//Type of Account 
+	float balance;			//Balance amount in the account
+	
+	public:
+	//Data Functions
+	values(string n,int acno,string type,int bal)	//To assign values 
+	{
+		name = n;			 
+		accountno = acno;	 
+		account_type = type;	 
+		balance = bal;
+	}	
+	
+	deposit()				//To deposited an amount 
+	{
+		float depo;
+		cout<<endl<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
+		cout<<"Enter deposit amount   : Rs.";
+		cin>>depo;
+		balance += depo;	
+		cout<<"Deposit successfully. "<<endl<<"Current balance : Rs."<<balance<<endl;
+	}	
+	
+	withdraw()				//To withdraw an amount after checking balance 
+	{
+		float with;
+		cout<<endl<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
+		cout<<"Enter Withdeaw amount  : Rs.";
+		cin>>with;
+		if(with<=balance)	//check balance available or not.
+		{
+			balance -= with;
+			cout<<"After withdraw : "<<endl<<"Current balance : Rs."<<balance<<endl;
+		}
+		else
+		{
+			cout<<"Insufficient balance.";
+			cout<<endl<<"Current balance : Rs."<<balance<<endl;
+		}
+	}
+	
+	display()			//To display name and balance 
+	{
+		cout<<endl<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
+		cout<<"Account Holder's Name  : "<<name<<endl;
+		cout<<"Account Number         : "<<accountno<<endl;
+		cout<<"Account Type           : "<<account_type<<endl;
+		cout<<"Account Balance        : Rs."<<balance;
+		cout<<endl<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
+	}
+};
+
+main()
+{
+	Bank ac; 		//create object 
+	
+	string name,type;
+	int accountno,balance;
+	
+					//user inputs
+	cout<<"******   Enter your details   ******"<<endl;
+		cout<<"Enter Depositor's Name : ";
+		getline(cin,name);
+		cout<<"Enter Account Number   : ";
+		cin>>accountno;
+		cout<<"Enter Account Type     : ";
+		cin>>type;
+		cout<<"Enter Account Balance  : Rs.";
+		cin>>balance; 
+		
+		//call all member functions.
+	ac.values(name,accountno,type,balance);		
+	ac.deposit();
+	ac.withdraw();
+	ac.display();
+}
+
+
